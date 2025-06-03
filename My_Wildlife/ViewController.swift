852cfebb27942c2ad6dc58258b7116a9827fe57c
Beytullah <beytul.1907_fb@icloud.com>
2025-06-03 15:23:37 +0300

    Initial Commit

---
+++ My_Wildlife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My_Wildlife
+//
+//  Created by Beytullah Cakmak on 3.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
