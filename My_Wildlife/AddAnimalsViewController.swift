a1325da784e84b365a71407d2b292b0dc5026454
Beytullah <beytul.1907_fb@icloud.com>
2025-06-08 14:26:32 +0300

    first commit

---
+++ My_Wildlife/AddAnimalsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddAnimalsViewController.swift
+//  My_Wildlife
+//
+//  Created by Beytullah Cakmak on 3.06.2025.
+//
+
+import UIKit
+
+class AddAnimalsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
